cec8eafe91c6c2ec6e46b3fd78f81df06d1c56a4
guillermodlmv <guillermodevillagomez@gmail.com>
2024-11-18 17:08:28 -0600

    Se implementa configuracion de seguridad

--- src/main/java/com/guillermodelamora/portafolioPersonal/security/SecurityConfig.java
+++ src/main/java/com/guillermodelamora/portafolioPersonal/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.guillermodelamora.portafolioPersonal.security;
 
 import com.guillermodelamora.portafolioPersonal.security.jwt.JwtFilter;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +15,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,14 +40,28 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
-                        .requestMatchers("/contacts", "/notices", "/register").permitAll()
+                        .requestMatchers("/user/login", "/user/signup", "/user/forgotPassword")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .cors(
+                        httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource((request) -> {
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                            config.setAllowedMethods(Collections.singletonList("*"));
+                            config.setAllowCredentials(true);
+                            config.setAllowedHeaders(Collections.singletonList("*"));
+                            config.setExposedHeaders(List.of("Authorization"));
+                            config.setMaxAge(3600L);
+
+                            return config;
+                        })
                 );
-        httpSecurity.formLogin(withDefaults());
-        httpSecurity.httpBasic(withDefaults());
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
