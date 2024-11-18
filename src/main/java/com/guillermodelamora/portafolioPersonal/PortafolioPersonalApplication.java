4c35b95f9131a820fe131c16a336349363ec6d5c
guillermodlmv <guillermodevillagomez@gmail.com>
2024-11-18 12:38:03 -0600

    initial commit

---
+++ src/main/java/com/guillermodelamora/portafolioPersonal/PortafolioPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.guillermodelamora.portafolioPersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guillermodelamora/portafolioPersonal/PortafolioPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillermodelamora.portafolioPersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
